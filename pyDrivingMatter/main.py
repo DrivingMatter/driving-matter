cb658516ff90c0dabb282dd29408eff6b3a3c0ae
Owais <soachishti@outlook.com>
2017-10-16 23:39:46 +0500

    WS Setup

---
+++ pyDrivingMatter/main.py
@@ -0,0 +1,48 @@
+from pyDrivingMatter import pyDrivingMatter
+from KBHit import KBHit
+
+with pyDrivingMatter() as pydm
+    cars = pydm.get_available_car()
+
+    if len(cars) == 0:
+        raise EnvironmentError("No car available")
+
+car_data = cars[0]
+
+car_link = "{}:{}".format(car_data['address'], car_data['port'])
+action_link = "{}/action"
+camera_c_link = "{}/camera_c"
+
+car = Car(action_link, camera_c_link)
+
+def handle_camera_c(data):
+    stream = io.BytesIO()
+    stream.write(data)
+    stream.seek(0)
+    img = np.asarray(Image.open(stream))
+    cv2.imshow("camera_c", img)
+    stream.seek(0)
+    key = cv2.waitKey(1) & 0xFF
+
+car.set_camera_c_callback(handle_camera_c)
+
+try:
+	kb = KBHit()
+    while True:
+        if kb.kbhit():
+            c = kb.carkey()
+            if c == 0: # Up
+                car.forward()            
+            elif c == 1: # Right
+                car.right() 
+            elif c == 2: # Down
+                car.backward()              
+            elif c == 3: # Left
+                car.left()              
+            elif c == 4: # Space
+                car.left()              
+
+
+finally:
+    kb.set_normal_term()    
+    cv2.destroyWindow("camera_c")
--- pyDrivingMatter/trash/pyDrivingMatter.old.py
+++ pyDrivingMatter/trash/pyDrivingMatter.old.py
@@ -134,4 +134,152 @@ class Car():
         self.camera_r_callback = callback
 
     def set_sensor_callback(self, callback)
-        self.sensor_callback = callbac
+        self.sensor_callback = callback
+
+"""
+class CameraOne(object):
+    def __init__(self, url, timeout):
+        self.url = url
+        self.timeout = timeout
+        self.ioloop = IOLoop.instance()
+        self.ws = None
+        self.connect()
+        #PeriodicCallback(self.keep_alive, 20000, io_loop=self.ioloop).start()
+        self.ioloop.start()
+
+    @gen.coroutine
+    def connect(self):
+        print "trying to connect"
+        try:
+            self.ws = yield websocket_connect(self.url)
+        except Exception, e:
+            print "connection error"
+        else:
+            print "connected"
+            self.ws.write_message("read_camera")
+            self.run()
+
+    @gen.coroutine
+    def run(self):
+        stream = io.BytesIO()
+        import time
+        start = time.time()
+        i = 0
+        elapse = 1 
+        while True:
+            
+            data = yield self.ws.read_message()
+            
+            if data is None:
+                print "connection closed"
+                self.ws = None
+                break
+
+            stream.write(data)
+            stream.seek(0)
+            img = np.asarray(Image.open(stream))
+            cv2.imshow("Frame", img)
+            stream.seek(0)
+            key = cv2.waitKey(1) & 0xFF
+
+            i += 1
+            elapse = (time.time() - start)
+            if elapse > 300000:
+                break;
+
+        cv2.destroyWindow("Frame")
+        print (i)
+        print (elapse)
+        print (str(i/elapse) + " fps")
+
+    def keep_alive(self):
+        if self.ws is None:
+            self.connect()
+        else:
+            self.ws.write_message("keep alive")
+
+
+class Action(object):
+    def __init__(self, url, timeout):
+        self.url = url
+        self.timeout = timeout
+        self.ioloop = IOLoop.instance()
+        self.ws = None
+        self.connect()
+        #PeriodicCallback(self.keep_alive, 20000, io_loop=self.ioloop).start()
+        self.ioloop.start()
+
+    @gen.coroutine
+    def connect(self):
+        print "trying to connect"
+        try:
+            self.ws = yield websocket_connect(self.url)
+        except Exception, e:
+            print "connection error"
+        else:
+            print "connected"
+            self.run()
+
+    @gen.coroutine
+    def run(self):
+        kb = KBHit()
+        sent = time()
+        tmp = -1
+        while True:
+            #sleep(0.1)
+            if kb.kbhit():
+                sent = time()
+                c = kb.carkey()
+                
+                if c == 0: # Up
+                    print ("forward")
+                    self.ws.write_message("forward")              
+                elif c == 1: # Right
+                    print ("forwardRight")
+                    self.ws.write_message("forwardRight")              
+                elif c == 2: # Down
+                    print ("backward")
+                    self.ws.write_message("backward")              
+                elif c == 3: # Left
+                    print ("forwardLeft")
+                    self.ws.write_message("forwardLeft")              
+                elif c == 4: # Space
+                    print ("stop")
+                    self.ws.write_message("stop")              
+                tmp = c
+
+            # if no signal for 1 second then stop the car and car not already stoped
+            elapse = time() - sent
+            if tmp != 4 and elapse > 0.1:
+                print (elapse)
+                self.ws.write_message("stop")                  
+                tmp = 4;
+
+        kb.set_normal_term()    
+        # if the `q` key was pressed, break from the loop
+        #if key == ord("q"):
+        #data = yield self.ws.read_message()
+        
+        #if data is None:
+        #    print "connection closed"
+        #    self.ws = None
+        #    break
+
+    def keep_alive(self):
+        if self.ws is None:
+            self.connect()
+        else:
+            self.ws.write_message("keep alive")
+
+
+
+if __name__ == "__main__":
+
+    camera = CameraOne("ws://192.168.43.183:8000/CameraOne", 5)
+    print ("here")
+    action = Action("ws://192.168.43.183:8000/Action", 5)
+    print ("After Action")
+    #camera = CameraOne("ws://192.168.1.11:8000/Sensor", 5)
+    
+    #camera = Client("ws://192.168.137.2:8000/CameraOne", 5)
+"""
