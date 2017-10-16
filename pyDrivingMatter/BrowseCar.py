3384356551e32306d784a58c2832deb811659c25
Owais <soachishti@outlook.com>
2017-10-16 10:50:01 +0500

    Added basic functions

---
+++ pyDrivingMatter/BrowseCar.py
@@ -0,0 +1,68 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+import socket
+import sys
+from time import sleep
+from zeroconf import ServiceBrowser, ServiceStateChange, Zeroconf
+
+logging.basicConfig(level=logging.DEBUG)
+
+class BrowseCar():
+
+    def __init__(self):
+        self.available_cars = []
+        self.zeroconf = None
+
+    def on_service_state_change(self, zeroconf, service_type, name, state_change):
+        logging.debug("Service %s of type %s state changed: %s" % (name, service_type, state_change))
+
+        if state_change is ServiceStateChange.Added:
+            self.info = zeroconf.get_service_info(service_type, name)
+            if self.info:
+                data = {};
+                data['service_name'] = self.info.name
+                data['address'] = socket.inet_ntoa(self.info.address)
+                data['port'] = self.info.port
+
+                if self.info.properties:
+                    for key, value in self.info.properties.items():
+                        data[key] = value
+                self.available_cars.append(data)
+            else:
+                logging.debug("No info in service name: " + name)
+        elif state_change is ServiceStateChange.Removed:
+            for car in self.available_cars:
+                if car['service_name'] == name:
+                    self.available_cars.remove(car)
+                    break
+
+    def get_available_car(self):
+        return self.available_cars
+
+    def start_browser(self, host = "_http._tcp.local."):
+        
+        self.zeroconf = Zeroconf()
+        logging.info("Browsing services")
+        browser = ServiceBrowser(self.zeroconf, host, handlers=[self.on_service_state_change])
+
+    def stop_browser(self):
+        if self.zeroconf:
+            self.zeroconf.close()    
+            logging.info("Zerconf browser closed")
+        else:
+
+            logging.error("Zeroconf not initiated")
+
+
+"""
+bc = BrowseCar()
+try:
+    bc.start_browser()
+    while True:
+        sleep(0.1)
+except KeyboardInterrupt:
+    pass
+finally:
+    bc.stop_browser()
+"""
--- pyDrivingMatter/pyDrivingMatter.py
+++ pyDrivingMatter/pyDrivingMatter.py
@@ -9,7 +9,124 @@ import cv2
 import numpy as np
 from PIL import Image
 from time import time, sleep
+
 from KBhit import KBHit
+from BrowseCar import BrowseCar
+
+class pyDrivingMatter():
+    def __init__(self):
+        self.bc = BrowseCar()
+        self.bc.start_browser()
+
+    def available_cars(self):
+        return self.bc.get_available_car()
+
+    # Destructor
+    def __enter__(self)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback)
+        bc.stop_browser()
+
+class Car():
+    def __init__(self, url_action, url_camera_c = None, url_camera_l = None, url_camera_r = None):
+        self.ws_action = yield websocket_connect(url_action) if url_action else None
+        self.ws_camera_c = yield websocket_connect(url_camera_c) if url_camera_c else None
+        self.ws_camera_l = yield websocket_connect(url_camera_l) if url_camera_l else None
+        self.ws_camera_r = yield websocket_connect(url_camera_r) if url_camera_r else None
+        
+        self.car_message_listener = None
+        self.camera_c_listener = None
+        self.camera_l_listener = None
+        self.camera_r_listener = None
+        self.sensor_listener = None
+
+    def sendStep(self, method, step)
+        current_time_epoch = int(time())
+        self.ws_action.write_message("{} {} {}".format(method, step, current_time_epoch))
+
+    def forward(self, step = 1)
+        self.sendStep("forward", step)
+
+    def forwardRight(self, step = 1)
+        self.sendStep("forwardRight", step)
+    
+    def forwardLeft(self, step = 1)
+        self.sendStep("forwardLeft", step)
+    
+    def backward(self, step = 1)
+        self.sendStep("backward", step)
+    
+    def backwardRight(self, step = 1)
+        self.sendStep("backwardRight", step)
+
+    def backwardLeft(self, step = 1)
+        self.sendStep("backwardLeft", step)
+
+    def stop(self, step = 1)
+        self.sendStep("stop", step)
+
+    def _car_message_listener(self, message):
+        """This method will be callback for websocket response of car.
+        """
+        if self.car_message_listener is not None:
+            self.car_message_listener(message)
+
+    def _camera_c_listener(self, message):
+        """This method will be callback for websocket response of car.
+        """
+        if self.camera_c_listener is not None:
+            self.camera_c_listener(message)
+
+    def _camera_l_listener(self, message):
+        """This method will be callback for websocket response of car.
+        """
+        if self.camera_l_listener is not None:
+            self.camera_l_listener(message)
+
+    def _camera_r_listener(self, message):
+        """This method will be callback for websocket response of car.
+        """
+        if self.camera_r_listener is not None:
+            self.camera_r_listener(message)
+
+    def _sensor_listener(self, message):
+        """This method will be callback for websocket response of car.
+        """
+        if self.sensor_listener is not None:
+            self.sensor_listener(message)
+
+    def set_car_message_listener(self, func):
+        self.car_message_listener = func
+
+    def set_camera_c_listener(self, func):
+        self.camera_c_listener = func
+
+    def set_camera_l_listener(self, func):
+        self.camera_l_listener = func
+
+    def set_camera_r_listener(self, func):
+        self.camera_r_listener = func
+
+    def set_sensor_listener(self, func)
+        self.sensor_listener = func
+
+
+
+pydm = pyDrivingMatter()
+cars = pydm.get_available_car()
+
+if len(cars) == 0:
+    raise EnvironmentError("No car available")
+
+car_data = cars[0]
+
+car_link = "{}:{}".format(car_data['address'], car_data['port'])
+action_link = "{}/action"
+camera_c_link = "{}/camera_c"
+
+car = Car(action_link, camera_c_link)
+
 
 class CameraOne(object):
     def __init__(self, url, timeout):
@@ -152,7 +269,8 @@ class Action(object):
 
 if __name__ == "__main__":
 
-    #camera = CameraOne("ws://192.168.43.183:8000/CameraOne", 5)
+    camera = CameraOne("ws://192.168.43.183:8000/CameraOne", 5)
+    print ("here")
     action = Action("ws://192.168.43.183:8000/Action", 5)
     print ("After Action")
     #camera = CameraOne("ws://192.168.1.11:8000/Sensor", 5)
