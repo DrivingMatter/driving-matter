268cc2d853c3d6e7a0b08bea1bb2eb02b39f12d0
Syed Owais Ali Chishti <soac@outlook.com>
2017-11-19 00:02:37 +0500

    Setup Camera

--- pyDrivingMatter/main.py
+++ pyDrivingMatter/main.py
@@ -19,24 +19,27 @@ from time import time
 
 cars = None
 
-with pyDrivingMatter() as pydm:
-    while True:
-        cars = pydm.available_cars()
-        if len(cars) == 0:
-            logging.debug("Waiting for cars on network")
-        else: 
-            break
-        sleep(1)
+# with pyDrivingMatter() as pydm:
+#     while True:
+#         cars = pydm.available_cars()
+#         if len(cars) == 0:
+#             logging.debug("Waiting for cars on network")
+#         else: 
+#             break
+#         sleep(1)
 
-car_data = cars[0]
+# car_data = cars[0]
 
-if len(sys.argv) == 2 and sys.argv[1] == "find_my_car":
-    logging.debug ("="*80)
-    logging.debug (car_data)
-    logging.debug ("="*80)
-    sys.exit()
+# if len(sys.argv) == 2 and sys.argv[1] == "find_my_car":
+#     logging.debug ("="*80)
+#     logging.debug (car_data)
+#     logging.debug ("="*80)
+#     sys.exit()
 
-car_link = "ws://{}:{}".format(car_data['address'], car_data['port'])
+#car_link = "ws://{}:{}".format(car_data['address'], car_data['port'])
+#car_link = "ws://{}:{}".format("192.168.137.2", "8000")
+car_link = "ws://{}:{}".format("192.168.0.120", "8000")
+
 action_link = "{}/action".format(car_link)
 state_link = "{}/state".format(car_link)
 
@@ -48,34 +51,44 @@ car = Car(action_link, url_state=state_link)
 start_time = int(time())
 total_requests = 1
 dataset = Dataset()
+previous_datavector = None
 
 def handle_state(data, ws):
-    global total_requests, start_time, dataset
+    global total_requests, start_time, dataset, previous_datavector
+    current_datavector = pickle.loads(data)
+    
+    total_requests += 1
+    
+    print ("Total requests: " + str(total_requests) + "\t\t" + str(int(time())))
 
-    #total_requests += 1
+    # request_per_sec = 0
+    # elapsed = int(time()) - start_time
 
-    #elapsed = int(time()) - start_time
+    # if elapsed == 0:
+    #     request_per_sec = total_requests / elapsed
+    
+    # if elapsed > 5:
+    #     total_requests = request_per_sec
+    #     start_time = int(time())        
 
-    #request_per_sec = total_requests / elapsed
+    # print ("RPS: " + str(request_per_sec))
 
-    #print ("RPS: " + str(request_per_sec))
+    sensors = current_datavector['sensors']
+    
+    #print (sensors)
 
-    print (int(time()))
-
-    data = pickle.loads(data)
-    sensors = data['sensors']
-
-    datavector = sensors    
-
-    camera_names = [key for key in data if key.startswith('camera')]
+    camera_names = [key for key in current_datavector if key.startswith('camera')]
     for name in camera_names:
-        img = Image.open(io.BytesIO(data[name]))
-        datavector[name] = img
+        frame = current_datavector[name]
+        if frame != None:
+            img = Image.open(io.BytesIO(frame))
+            
+            cv2.imshow(name, np.asarray(img))
+            cv2.waitKey(1) # CV2 Devil - Don't dare to remove
+        else:
+            logging.debug("None frame received. Camera: " + name)
 
-        cv2.imshow(name, np.asarray(img))
-        cv2.waitKey(1) # CV2 Devil - Don't dare to remove
-
-    dataset.save_data(datavector)
+    #dataset.save_data(datavector)
 
 car.set_state_callback(handle_state)
 
@@ -97,4 +110,4 @@ try:
                 car.stop()              
 finally:
     kb.set_normal_term()    
-    #cv2.destroyWindow("camera_c")
+    cv2.destroyAllWindow()
